855ef13fa1bdaed131e3126fa1e99de158f630a1
Lakshya Taragi <72077704+lakshyaTaragi@users.noreply.github.com>
2020-10-26 15:15:26 +0530

    Create selectionSort.py

---
+++ selectionSort/selectionSort.py
@@ -0,0 +1,14 @@
+def selection_sort(arr):
+    for i in range(len(arr)):
+       minim= i
+       for j in range(i+1, len(arr)):
+          if arr[minim] > arr[j]:
+             minim = j
+       arr[i], arr[minim] = arr[minim], arr[i]
+    for j in range(len(arr)):
+        print(arr[j])
+    
+    
+selection_sort([5,7,9,0,4])
+print('')
+selection_sort(['i','n','s','e','r','t','i','o','n'])
